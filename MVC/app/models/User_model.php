f1e36a8530154c19f1df6a5cb9dbe15fbdce11ad
frking69 <121990793+frking69@users.noreply.github.com>
2024-10-28 21:07:46 +0700

    Add files via upload

---
+++ MVC/app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_model
+{
+    private $nama = 'Muhammad Oding Kacadek';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
---
+++ MVC/app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>img/Oding.jpg" alt="Muhammad Oding Kacadek" width="250" class="rounded-circle shadow">
+    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> Tahun, saya seorang <?= $data['pekerjaan']; ?></p>
+</div>
